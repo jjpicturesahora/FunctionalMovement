b3084289bc52ffd222c48f58604e5b9467e4e698
JJPictures Ahora <134988146+jjpicturesahora@users.noreply.github.com>
2023-06-15 21:15:10 +0000

    Add

--- comportamiento/code.js
+++ comportamiento/code.js
@@ -1,10 +1,11 @@
-var header = document.getElementById('Header')
+// var header = document.getElementById('Header')
 
-window.addEventListener('scroll',()=>{
-    var scroll=window.scrollY
-    if(scroll>10){
-        header.style.backgroundColor= '#121212'
-    }else{
-        header.style.backgroundColor='transparent'
-    }
-}) 
+// window.addEventListener('scroll',()=>{
+//     var scroll=window.scrollY
+//     if(scroll>10){
+//         header.style.backgroundColor= '#121212'
+//     }else{
+//         header.style.backgroundColor='transparent'
+//     }
+// }) 
+//14.06.23
